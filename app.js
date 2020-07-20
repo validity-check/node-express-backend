258f4f81961a6b949dc449d9b6db7e1ba661a2fa
validity-check <mzaiddar77@gmail.com>
2020-07-20 15:56:47 +0100

    Nodemon & Error Handling & Morgan

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
-const produtRoutes = require('./api/routes/products');
-const orderRoutes = require('./api/routes/orders');
+const produtRoutes = require("./api/routes/products");
+const orderRoutes = require("./api/routes/orders");
+
+// Morgan
+app.use(morgan("dev"));
 
 // Handle routes
-app.use('/products', produtRoutes);
-app.use('/orders', orderRoutes)
+app.use("/products", produtRoutes);
+app.use("/orders", orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found!");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
